9f7da26a9c28fedd82c75ffdefb4190228b559fc
Student D07 EE364 <ee364d07@ecegrid-thin8.ecn.purdue.edu>
2016-04-10 16:48:16 -0400

    [Working]

--- Lab11/Steganography.py
+++ Lab11/Steganography.py
@@ -3,47 +3,72 @@ import sys
 import scipy
 import zlib
 import base64
+import re
 
 class Payload:
     def __init__(self, img=None, compressionLevel=-1, xml=None):
         self.img = img
+        self.cmplvl = compressionLevel
         self.xml = str(xml)
-        #print(img)
-        #print(compressionLevel)
-        #print(xml)
 
         if(img is None and xml is None):
             raise ValueError("Image and XML are not provided.")
+
         if compressionLevel > 9 or compressionLevel < -1:
             raise ValueError("Compression Level has invalid value")
+
         if(xml is not None and type(xml) is not str):
             raise TypeError("XML has incorrect type")
+
         if(type(img) is not np.ndarray):
             raise TypeError("Image has incorrect type")
 
+        # Image is given but XML is not.
         if (img is not None and xml is None):
-            if(compressionLevel != -1):
-                compressed = "True"
-                value_1=zlib.compress(img, compressionLevel)
-            else:
-                value_1=self.img
-                compressed = "False"
-            cmp_value=base64.b64encode(value_1)
-                                            # Compressed retreived
-            row,col,num_byte = img.shape
-            img_size=""
-            img_size+=str(row)
-            img_size+=","
-            img_size+=str(col)
-                                            # Image Size retreived
-            if(num_byte == 1):
-                payload_type = "Color"
-            elif(num_byte == 3):
-                payload_type = "Gray"
-                                            # Payload Type retreived
+            raster_array = raster_scan(self)
+            extractXML(self, raster_array)
 
-            self.xml='<?xml version="1.0" encoding="UTF-8"?>\n'
-            self.xml+='<payload type="'+payload_type+'" size="'+img_size+'" compressed="'+compressed+'">\n'
-            self.xml+=str(cmp_value, encoding='UTF-8')+'\n'
-            self.xml+='</payload>'
+        if (img is None and xml is not None):
 
+
+def raster_scan(self):
+    if(len(self.img.shape) == 3):
+        comb_list = self.img.transpose(2, 0, 1)
+    else:
+        comb_list = self.img
+    comb_list = comb_list.flatten()
+    return(np.asarray(comb_list))
+
+def extractXML(self, raster_array):
+
+    # Compressed retreived
+
+    if(self.cmplvl != -1):
+        compressed = "True"
+        value_1=zlib.compress(raster_array, self.cmplvl)
+    else:
+        value_1=self.img
+        compressed = "False"
+    cmp_value=base64.b64encode(value_1)
+
+
+    # Image Size retreived
+
+    size_chk= self.img.shape
+    img_size=""
+    img_size+=str(size_chk[0])
+    img_size+=","
+    img_size+=str(size_chk[1])
+
+
+    # Payload Type retreived
+
+    if(len(size_chk) == 3):
+        payload_type = "Color"
+    else:
+        payload_type = "Gray"
+
+    self.xml='<?xml version="1.0" encoding="UTF-8"?>\n'
+    self.xml+='<payload type="'+payload_type+'" size="'+img_size+'" compressed="'+compressed+'">\n'
+    self.xml+=str(cmp_value, encoding='UTF-8')+'\n'
+    self.xml+='</payload>'
