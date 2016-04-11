ab29aec979d26519860c95b5e0dc0ee1f81c0f3d
Student D07 EE364 <ee364d07@ecegrid-thin8.ecn.purdue.edu>
2016-04-10 21:56:27 -0400

    [Done] Finally done with Payload class

--- Lab11/Steganography.py
+++ Lab11/Steganography.py
@@ -20,15 +20,14 @@ class Payload:
         if(xml is not None and type(xml) is not str):
             raise TypeError("XML has incorrect type")
 
-        if(type(img) is not np.ndarray):
+        if(img is not None and type(img) is not np.ndarray):
             raise TypeError("Image has incorrect type")
 
-        # Image is given but XML is not.
-        if (img is not None and xml is None):
+        if (xml):
+            extractIMG(self)
+        else:
             raster_array = raster_scan(self)
-            extractXML(self, raster_array)
-
-        if (img is None and xml is not None):
+            generateXML(self, raster_array)
 
 
 def raster_scan(self):
@@ -39,36 +38,83 @@ def raster_scan(self):
     comb_list = comb_list.flatten()
     return(np.asarray(comb_list))
 
-def extractXML(self, raster_array):
-
-    # Compressed retreived
-
+def generateXML(self, raster_array):
+    #--------------------------------------------------------#
+    # Compressing
+    #--------------------------------------------------------#
     if(self.cmplvl != -1):
         compressed = "True"
         value_1=zlib.compress(raster_array, self.cmplvl)
     else:
         value_1=self.img
         compressed = "False"
+
+    #--------------------------------------------------------#
+    # Base 64 encoding
+    #--------------------------------------------------------#
     cmp_value=base64.b64encode(value_1)
 
-
+    #--------------------------------------------------------#
     # Image Size retreived
-
+    #--------------------------------------------------------#
     size_chk= self.img.shape
     img_size=""
     img_size+=str(size_chk[0])
     img_size+=","
     img_size+=str(size_chk[1])
 
-
+    #--------------------------------------------------------#
     # Payload Type retreived
-
+    #--------------------------------------------------------#
     if(len(size_chk) == 3):
         payload_type = "Color"
     else:
         payload_type = "Gray"
 
+    #--------------------------------------------------------#
+    # Generating XML
+    #--------------------------------------------------------#
     self.xml='<?xml version="1.0" encoding="UTF-8"?>\n'
     self.xml+='<payload type="'+payload_type+'" size="'+img_size+'" compressed="'+compressed+'">\n'
     self.xml+=str(cmp_value, encoding='UTF-8')+'\n'
     self.xml+='</payload>'
+
+def extractIMG(self):
+    #--------------------------------------------------------#
+    # Extracting everything from XML String
+    #--------------------------------------------------------#
+    img_str=self.xml.split('\n')[1]
+    xml_str=self.xml.split('\n')[2]
+    expr=r'<payload type="(.*)" size="(.*),(.*)" compressed="(.*)">'
+    matched = re.search(expr, img_str)
+    if matched:
+        payload_type=matched.group(1)
+        row=matched.group(2)
+        col=matched.group(3)
+        compressed=matched.group(4)
+
+    #--------------------------------------------------------#
+    # Base 64 decoding
+    #--------------------------------------------------------#
+    cmp_value=base64.b64decode(xml_str)
+
+    #--------------------------------------------------------#
+    # Decompress
+    #--------------------------------------------------------#
+    if(compressed == "True"):
+        dcmp_val=list(zlib.decompress(cmp_value))
+    else:
+        dcmp_val=list(cmp_value)
+
+    #--------------------------------------------------------#
+    # Creating IMG based on Color or Gray Scale
+    #--------------------------------------------------------#
+    if(payload_type == "Gray"):
+        self.img = np.resize(dcmp_val, (int(row),int(col)))
+    elif(payload_type == "Color"):
+        new_list=[0 for i in range(len(dcmp_val))]
+        for i in range(int(len(dcmp_val)/3)):
+            new_list[3*i]=dcmp_val[i]
+            new_list[3*i+1]=dcmp_val[int(len(dcmp_val)/3)+i]
+            new_list[3*i+2]=dcmp_val[int(len(dcmp_val)/3)*2+i]
+        self.img = np.resize(new_list, (int(row),int(col),3))
